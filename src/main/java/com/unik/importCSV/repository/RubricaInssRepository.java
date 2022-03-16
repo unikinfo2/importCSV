e54b09a670b3e4c5d4e0b6214c4594790acaa1dd
Antonio Carlos <64972615+unikinfo2@users.noreply.github.com>
2022-03-16 18:55:09 -0300

    Add files via upload

---
+++ src/main/java/com/unik/importCSV/repository/RubricaInssRepository.java
@@ -0,0 +1,10 @@
+package com.unik.importCSV.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unik.importCSV.model.RubricaInss;
+
+public interface RubricaInssRepository extends JpaRepository<RubricaInss, String>{
+
+}
---
+++ src/test/java/com/unik/importCSV/ImportCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unik.importCSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
